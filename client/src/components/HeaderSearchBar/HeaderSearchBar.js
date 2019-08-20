6cf86c2b1d1bf556ddbd34be920e9728131c2a05
Reeves <froningr@gmail.com>
2019-08-20 11:20:59 +0900

    create redux store and connected search bar

--- client/src/components/HeaderSearchBar/HeaderSearchBar.js
+++ client/src/components/HeaderSearchBar/HeaderSearchBar.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './HeaderSearchBar.css';
 import SearchBar from '../SearchBar/SearchBar'
 import logo_header from '../../logo_header.png';
 
-class HeaderSearchBar extends SearchBar {
+class HeaderSearchBar extends Component {
     constructor(props) {
         super(props);
         this.state = {
