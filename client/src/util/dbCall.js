b87feea2eaf33ece1c0afe28edf3ff04d88c5315
Reeves <froningr@gmail.com>
2019-08-26 19:50:10 +0900

    Remove console log code

--- client/src/util/dbCall.js
+++ client/src/util/dbCall.js
@@ -2,12 +2,10 @@ const dbCall = {
     search: (term, location) => {
         return fetch(`http://localhost:4000/results?term=${term}&loc=${location}`)
         .then((res) => {
-            console.log(JSON.stringify(res));
             return res.json();
         })
         .then((jsonRes) => {
             if (jsonRes.products) {
-                console.log(JSON.stringify(jsonRes));
                 return jsonRes.products.map((product) => {
                     return product;
                 });
