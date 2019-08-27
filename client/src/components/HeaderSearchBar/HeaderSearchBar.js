db58e0f36a1d13a7eb118a2324cf948e3356368c
Reeves <froningr@gmail.com>
2019-08-27 16:58:22 +0900

    Remove state, reset redux when return to home

--- client/src/components/HeaderSearchBar/HeaderSearchBar.js
+++ client/src/components/HeaderSearchBar/HeaderSearchBar.js
@@ -1,24 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 
 import { SearchBar } from '../SearchBar/SearchBar';
+
 import { connect } from 'react-redux';
-import { termChange, locChange } from '../../actions/actions'
-import { searchState } from '../../util/searchState';
+import { termChange, locChange, resetState } from '../../redux/actions';
+import { searchStateMap } from '../../redux/util/searchStateMap';
+
 import logo_header from '../../logo_header.png';
 import './HeaderSearchBar.css';
 
-
+//Component Class
 class HeaderSearchBar extends SearchBar {
     constructor(props) {
         super(props);
-        this.state = {
-            term: this.props.term,
-            loc: this.props.loc
-        }
+
+        this.handleHomeLink = this.handleHomeLink.bind(this);
     }
     
+    //Reset Redux State when returning to home page
+    handleHomeLink(e) {
+        this.props.resetState();
+    }
+
     render () {
         return (
             <React.Fragment>
@@ -29,33 +34,40 @@ class HeaderSearchBar extends SearchBar {
                     content={ `Foreign Prodcut Reference Web Application Results for query'` + /*Search Term*/ + `'.` } 
                   />
                 </Helmet>
-                <div className='pure-u-1 pure-u-md-3-8' >
-                    <input autoFocus 
-                        defaultValue={this.props.term}
-                        onChange={this.handleProductChange} 
-                        onKeyPress={this.handleEnter}
-                        style={searchBarHeaderStyle} 
-                        />
-                </div>
-                <div className='pure-u-1-2 pure-u-md-1-8' onClick={this.handleSearch}>
-                    <Link className='pure-button pure-button-primary'
-                        to={{
-                            pathname: '/results',
-                            search: `?term=${this.props.term}&loc=${this.props.loc}`
-                        }}
-                        style={searchSubmitHeaderStyle}
-                        >
-                        Let's go
-                    </Link>
-                </div> 
-                <div className='pure-u-1-2 pure-u-md-3-8 switch-header' >
-                    <label className='loc-tag-header' style={langLabelLeftHeaderStyle}>JP</label>
-                    <label className='lang-switch-header' style={switchHeaderStyles}>
-                        <input type='checkbox' checked={this.props.loc === 'us' ? 'checked' : ''}
-                            onChange={this.handleLocationChange}/>
-                        <span className='slider-round-header'></span>
-                    </label>
-                    <label className='loc-tag-header' style={langLabelRightHeaderStyle}>US</label>
+                <div className='pure-u-1' style={headerBarStyle}>
+                    <div className='pure-u-1 pure-u-md-1-8' >
+                        <Link to='/' >
+                            <img src={logo_header} alt='FoPoRef' style={miniLogoStyle} onClick={this.handleHomeLink}/>
+                        </Link>
+                    </div>
+                    <div className='pure-u-1 pure-u-md-3-8' >
+                        <input autoFocus 
+                            defaultValue={this.props.term}
+                            onChange={this.handleProductChange} 
+                            onKeyPress={this.handleEnter}
+                            style={searchBarHeaderStyle} 
+                            />
+                    </div>
+                    <div className='pure-u-1-2 pure-u-md-1-8' onClick={this.handleSearch}>
+                        <Link className='pure-button pure-button-primary'
+                            to={{
+                                pathname: '/results',
+                                search: `?term=${this.props.term}&loc=${this.props.loc}`
+                            }}
+                            style={searchSubmitHeaderStyle}
+                            >
+                            Let's go
+                        </Link>
+                    </div> 
+                    <div className='pure-u-1-2 pure-u-md-3-8 switch-header' >
+                        <label className='loc-tag-header' style={langLabelLeftHeaderStyle}>JP</label>
+                        <label className='lang-switch-header' style={switchHeaderStyles}>
+                            <input type='checkbox' checked={this.props.loc === 'us' ? 'checked' : ''}
+                                onChange={this.handleLocationChange}/>
+                            <span className='slider-round-header'></span>
+                        </label>
+                        <label className='loc-tag-header' style={langLabelRightHeaderStyle}>US</label>
+                    </div>
                 </div>
             </React.Fragment>
         );
@@ -108,8 +120,15 @@ const switchHeaderStyles = {
     margin: '0.5em 0 0 0',
 }
 
+// Map Dispatch to Props
+const mapDispatchToProps = (dispatch) => ({
+    termChange: (term) => {dispatch(termChange(term))},
+    locChange: (loc) => {dispatch(locChange(loc))},
+    resetState: () => {dispatch(resetState())}
+});
+
 // Wrapping Up
 export default connect(
-    searchState,
-    {termChange, locChange},
+    searchStateMap,
+    mapDispatchToProps,
     )(HeaderSearchBar);
