f60cc0f2d348cf61ba05f535d6adeb2acc8b7d8b
Reeves <froningr@gmail.com>
2019-08-27 16:56:40 +0900

    Move store creation to seperate file

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import searchState from './reducers/reducers'
+import store from './redux/store';
 import './index.css';
 import App from '../src/components/App/App';
 import * as serviceWorker from './serviceWorker';
-
-const store = createStore(searchState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render((
     <Provider store={store}>
