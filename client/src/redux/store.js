f6d040203af8cef08c74282fe6445b7af4fef3b3
Reeves <froningr@gmail.com>
2019-09-07 11:29:01 +0900

    Rework store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,5 +1,10 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import  searchState from './reducers/searchState';
+import  rootReducer from './reducers';
 
-export default createStore(searchState, applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+export default createStore(rootReducer,
+    compose(
+        applyMiddleware(thunk),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+);
